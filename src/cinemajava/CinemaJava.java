3e588146cc9853413212157298b884590a230828
Halejandro Acevedo <halejandro2986@gmail.com>
2024-10-04 20:43:17 -0500

    Primer commit con la estructura del proyecto

---
+++ src/cinemajava/CinemaJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cinemajava;
+
+/**
+ *
+ * @author halejandro
+ */
+public class CinemaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
